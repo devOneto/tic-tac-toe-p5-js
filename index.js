8347ab57e822cc5a1b74c131022705637fcfe943
Neto Araujo <bipnerds@gmail.com>
2021-08-15 11:33:43 -0300

    Configuração Inicial

---
+++ index.js
@@ -0,0 +1,16 @@
+function setup(){
+    createCanvas(800,600);
+}
+
+function draw(){
+    background(0);
+
+    
+    fill(255);
+    textSize(30);
+    text("Jogo da Velha",280,40);
+
+    //tabletop
+    
+
+}
