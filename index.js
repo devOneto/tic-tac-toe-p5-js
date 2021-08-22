f80e4bec4dd69b8a78ceeb0b395401a50dcaaf6d
Neto Araujo <bipnerds@gmail.com>
2021-08-21 21:01:09 -0300

    Tabuleiro com click nas coordenadas.

--- index.js
+++ index.js
@@ -4,9 +4,53 @@ estaDentroDoX = false;
 estaDentroDo0 = false;
 
 escolhaDoJogador = "nenhum";
+estaEscolhendo = true;
+
+quadroUm = null;
+quadroDois = null;
+quadroTres = null;
+quadroQuatro = null;
+quadroCinco = null;
+quadroSeis = null;
+quadroSete = null;
+quadroOito = null;
+quadroNove = null;
+
+quadros = null;
 
 function setup(){
+
     createCanvas(800,600);
+
+    quadroUm = new Quad(0,0);
+    quadroDois = new Quad(0,0);
+    quadroTres = new Quad(0,0);
+    quadroQuatro = new Quad(0,0);
+    quadroCinco = new Quad(0,0);
+    quadroSeis = new Quad(0,0);
+    quadroSete = new Quad(0,0);
+    quadroOito = new Quad(0,0);
+    quadroNove= new Quad(0,0);
+
+    quadros = [quadroUm,quadroDois,quadroTres,quadroQuatro,quadroCinco,quadroSeis,quadroSete,quadroOito,quadroNove]
+
+    sqX = 400 - 150;
+    sqY = 300 - 150;
+    sqL = 300;
+
+    noFill();
+    count = -1;
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            count++;
+            quadros[count].y = 200 + 80*i;
+            quadros[count].x = 200 + 80*j;
+            quadros[count].n = count;
+
+        }
+    }
+
+    
 }
 
 function draw(){
@@ -20,6 +64,8 @@ function draw(){
 
     if(escolhaDoJogador == "nenhum"){
         escolha();
+    } else {
+        partida();
     }
 
     if(escolhaDoJogador != "nenhum"){
@@ -28,6 +74,13 @@ function draw(){
     }
 
     //tabletop
+    count=-1;
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            count++;
+            quadros[count].draw();    
+        }     
+    }
     
 }
 
@@ -55,6 +108,8 @@ function escolha(){
         corFundoRetanguloX = 255;
         corX = 0;
         estaDentroDoX = true;
+    }else{
+        estaDentroDoX = false;
     }
 
     if(
@@ -65,6 +120,8 @@ function escolha(){
         corFundoRetangulo0 = 255;
         cor0 = 0;
         estaDentroDo0 = true;
+    }else{
+        estaDentroDo0 = false;
     }
 
     text("Escolha com o que você deseja jogar!", 150, 120);
@@ -83,23 +140,117 @@ function escolha(){
     fill(cor0);
     text("0", 540, 350);
 
-    textSize(20);
-    strokeWeight(0.5);
-    text("texto", mouseX, mouseY);
-
-
 }
 
 function mouseClicked(){
 
-    if(estaDentroDoX){
+    if(estaDentroDoX && estaEscolhendo){
         console.log("Está dentro do X e foi clicado.");
         escolhaDoJogador = "X";
+        estaEscolhendo = false;
     }
 
-    if(estaDentroDo0){
+    if(estaDentroDo0 && estaEscolhendo){
         console.log("Está dentro do 0 e foi clicado.");
         escolhaDoJogador = "0";
+        estaEscolhendo = false;
+    }
+
+    if (quadroUm.isHover){
+        console.log("Clicou no quadro: " + 0)
+    }
+
+    if (quadroDois.isHover){
+        console.log("Clicou no quadro: " + 1)
+    }
+    
+    if (quadroTres.isHover){
+        console.log("Clicou no quadro: " + 2)
+    }
+
+    if (quadroQuatro.isHover){
+        console.log("Clicou no quadro: " + 3)
+    }
+
+    if (quadroCinco.isHover){
+        console.log("Clicou no quadro: " + 4)
+    }
+
+    if (quadroSeis.isHover){
+        console.log("Clicou no quadro: " + 5)
+    }
+
+    if (quadroSete.isHover){
+        console.log("Clicou no quadro: " + 6)
+    }
+
+    if (quadroOito.isHover){
+        console.log("Clicou no quadro: " + 7)
+    }
+
+    if (quadroNove.isHover){
+        console.log("Clicou no quadro: " + 8)
+    }
+
+}
+
+function partida(){
+
+    quadroUm.draw();
+    quadroUm.listenClick();
+    quadroDois.draw();
+    quadroDois.listenClick();
+    quadroTres.draw();
+    quadroTres.listenClick();
+    quadroQuatro.draw();
+    quadroQuatro.listenClick();
+    quadroCinco.draw();
+    quadroCinco.listenClick();
+    quadroSeis.draw();
+    quadroSeis.listenClick();
+    quadroSete.draw();
+    quadroSete.listenClick();
+    quadroOito.draw();
+    quadroOito.listenClick();
+    quadroNove.draw();
+    quadroNove.listenClick();
+
+    strokeWeight(1);
+}
+
+class Quad{
+
+    constructor(x,y,w = 80,h=80,n=0){
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.n = 0;
+        this.isHover = false;
+    }
+
+    listenClick(){
+        if( 
+            (mouseX >= this.x && mouseX <= this.x + this.w) 
+            &&
+            (mouseY >= this.y && mouseY <= this.y + this.h)
+        ) {
+            this.isHover = true;
+        } else {
+            this.isHover = false;
+        }
+    }
+
+    draw(){
+        fill(255,0,0)
+        ellipse(this.x, this.y,5);
+        noStroke()
+        textSize(12)
+        text("(" + this.x + "," + this.y +")" , this.x,this.y-5)
+        stroke(255)
+        noFill();
+        rect(this.x,this.y,this.w,this.h);
+        text(this.n, this.x + this.w/2, this.y + this.h/2);
     }
 
 }
